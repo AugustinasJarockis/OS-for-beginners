0c31cd319aeaa2fff21470a0cb9b0db23ac56ea9
Arminas Brazenas <arminas@eldorado.gg>
2025-03-04 17:34:22 +0200

    Change

--- MachineEmulator/RAM.cs
+++ MachineEmulator/RAM.cs
@@ -2,10 +2,10 @@
 
 public class RAM
 {
-    private readonly byte[] _data;
-    
     public RAM(byte[] data)
     {
-        _data = data;
+        Data = data;
     }
+
+    public byte[] Data { get; }
 }
