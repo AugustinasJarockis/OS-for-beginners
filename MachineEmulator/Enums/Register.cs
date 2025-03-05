0ff4fa321a2b9e9a9b03642e905ca3bf045a7290
AugustinasJarockis <augustinas.jarockis@gmail.com>
2025-03-05 23:15:45 +0200

    Add decoder

---
+++ MachineEmulator/Enums/Register.cs
@@ -0,0 +1,9 @@
+﻿namespace MachineEmulator.Enums
+{
+    enum Register
+    {
+        R0, R1, R2, R3, R4, R5, R6, R7,
+        PC, FR, SP, PTBR,
+        Unknown
+    }
+}
--- MachineEmulator/RAM.cs
+++ MachineEmulator/RAM.cs
@@ -8,19 +8,19 @@ public class RAM
         Data = new int[RAM_SIZE];
     }
 
-    public byte this[int key] {
+    public byte this[long key] {
         get => GetValue(key);
         set => SetValue(key, value);
     }
 
-    private byte GetValue(int key) {
+    private byte GetValue(long key) {
         int dword = Data[key / 4];
-        return (byte)((dword >> ((3 - key % 4) * 8)) & 0xFF);
+        return (byte)((dword >> (int)((3 - key % 4) * 8)) & 0xFF);
     }
 
-    private void SetValue(int key, byte value) {
+    private void SetValue(long key, byte value) {
         int dwordToSet = Data[key / 4];
-        int shiftCount = ((3 - key % 4) * 8);
+        int shiftCount = ((int)(3 - key % 4) * 8);
         int mask = 0xFF << shiftCount;
         Data[key / 4] = (int)((dwordToSet & (0xFFFFFFFF ^ mask)) + (value << shiftCount));
     }
