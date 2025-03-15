95e5f52df9109cc855010362fcdb6cb38762da9f
Arminas Brazenas <arminas.brazenas@gmail.com>
2025-03-15 20:11:25 +0200

    Add bootloader

--- MachineEmulator/Program.cs
+++ MachineEmulator/Program.cs
@@ -1,12 +1,12 @@
-﻿using Assembler;
-using MachineEmulator;
+﻿using MachineEmulator;
 
-// var ram = new RAM("ramSnapshot.mem");
-var ram = new RAM();
-AppDomain.CurrentDomain.ProcessExit += (sender, eventArgs) => ram.Dispose();
+var ramSnapshotFilePath = Path.Combine(
+    Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData),
+    "vuos",
+    "ramSnapshot.mem");
 
-LoadInterruptHandlers(ram);
-LoadCode(ram);
+var ram = new RAM(ramSnapshotFilePath);
+AppDomain.CurrentDomain.ProcessExit += (_, _) => ram.Dispose();
 
 var interruptDevice = new HardwareInterruptDevice();
 
@@ -14,41 +14,5 @@ var processor = new Processor(
     ram,
     interruptDevice,
     periodicInterruptInterval: TimeSpan.FromSeconds(10));
+
 processor.Run();
-
-static void LoadInterruptHandlers(RAM ram)
-{
-    var addressByInterruptCode = new Dictionary<byte, uint>
-    {
-        [0] = 0x10000,
-        [1] = 0x10100,
-        [2] = 0x10200,
-        [3] = 0x10300,
-        [4] = 0x10400,
-        [5] = 0x10500,
-        [6] = 0x10600,
-        [7] = 0x10700
-    };
-
-    foreach (var (interruptCode, handlerAddress) in addressByInterruptCode)
-    {
-        ram.SetDWord((ulong)(interruptCode * 4), handlerAddress);
-        
-        var handlerFilePath = Path.Join(Environment.CurrentDirectory, "Data", "InterruptHandlers", $"{interruptCode}.txt");
-        var machineCode = MachineCodeAssembler.ToMachineCode(handlerFilePath);
-        for (var i = 0; i < machineCode.Count; i++)
-        {
-            ram.SetDWord((ulong)(handlerAddress + i * 4), machineCode[i]);
-        }
-    }
-}
-
-static void LoadCode(RAM ram)
-{
-    var codeFilePath = Path.Join(Environment.CurrentDirectory, "Data", "code.txt");
-    var machineCode = MachineCodeAssembler.ToMachineCode(codeFilePath);
-    for (var i = 0; i < machineCode.Count; i++)
-    {
-        ram.SetDWord((ulong)(0x508 + i * 4), machineCode[i]);
-    }
-}
--- MachineEmulator/RAM.cs
+++ MachineEmulator/RAM.cs
@@ -3,22 +3,35 @@
 namespace MachineEmulator;
 
 public class RAM : IDisposable
-{    
+{
+    private readonly string? _snapshotFilePath;   
+    
     public RAM()
     {
         Data = new uint[SizeConstants.RAM_SIZE];
     }
 
-    public RAM(string filePath) {
+    public RAM(string filePath)
+    {
         Data = new uint[SizeConstants.RAM_SIZE];
-        byte[] fileData = File.ReadAllBytes(filePath);
-        Buffer.BlockCopy(fileData, 0, Data, 0, fileData.Length);
+        _snapshotFilePath = filePath;
+        
+        if (File.Exists(filePath))
+        {
+            byte[] fileData = File.ReadAllBytes(filePath);
+            Buffer.BlockCopy(fileData, 0, Data, 0, fileData.Length);
+        }
     }
-    public void Dispose() {
-        FileStream stream = File.Open("ramSnapshot.mem", FileMode.Create);
-        byte[] writeData = new byte[SizeConstants.MB64];
-        Buffer.BlockCopy(Data, 0, writeData, 0, (int)SizeConstants.MB64);
-        stream.Write(writeData, 0, (int)SizeConstants.MB64);
+    
+    public void Dispose()
+    {
+        if (_snapshotFilePath is not null)
+        {
+            FileStream stream = File.Open(_snapshotFilePath, FileMode.Create);
+            byte[] writeData = new byte[SizeConstants.MB64];
+            Buffer.BlockCopy(Data, 0, writeData, 0, (int)SizeConstants.MB64);
+            stream.Write(writeData, 0, (int)SizeConstants.MB64);   
+        }
     }
     
     private uint[] Data { get; }
